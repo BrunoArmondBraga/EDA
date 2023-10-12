8abff6b8b925335e72ab001846c9cb36d0300bbe
BrunoArmondBraga <abraga3547bruno@gmail.com>
2023-10-12 01:25:01 -0300

    Adds header and iniciatial kinematicHeap implementation

--- teste.cpp
+++ teste.cpp
@@ -44,7 +44,11 @@ int MaxHeap::getMax() {
     if (heap.empty()) {
         throw std::runtime_error("Heap is empty.");
     }
-    return heap[0];
+    for(int i= 0; i < heap.size(); i++){
+        cout << "i = " << i << "      -> " << heap[i];
+        cout << endl; 
+    }
+    return heap[0] + 1;
 }
 
 bool MaxHeap::isEmpty() {
@@ -52,7 +56,9 @@ bool MaxHeap::isEmpty() {
 }
 
 void MaxHeap::print() {
+    cout << "Maxheap print!" << endl;
     recursive_print(0, 0);
+    cout << endl;
 }
 
 void MaxHeap::print_cru() {
@@ -97,6 +103,11 @@ void MaxHeap::updateKey(int index) {
     }
 }
 
+void MaxHeap::change_key(int id, int new_velocity){
+    TS->at(id*2) = new_velocity;
+    this->updateKey(id);
+}
+
 void MaxHeap::add_last_item() {
     insert(heap.size());
 }
@@ -135,20 +146,32 @@ void MaxHeap::troca_interna(int index1, int index2) {
     heap[index2] = aux;
 }
 
-void MaxHeap::recursive_print(int x, int back) {
-    int tempo = *t;
-    if (x < 0 || x >= heap.size()) {
+void MaxHeap::recursive_print(int x, int back){
+    if(x < 0 || x >= heap.size()){
         return;
     }
-
-    cout << "Index: " << x << " Position: " << TS->at(2 * heap[x]) << " Date: " << TS->at(2 * heap[x] + 1) * tempo << " Back: " << back << " Parent: " << TS->at(2 * heap[(x - 1) / 2]) << endl;
-
-    if (2 * x + 1 < heap.size()) {
-        recursive_print(2 * x + 1, x);
+    int tempo = *t;
+    recursive_print(2 * x + 1, back + 3);
+    for(int j = 0; j < back; j++){
+        cout << " ";
     }
-    if (2 * x + 2 < heap.size()) {
-        recursive_print(2 * x + 2, x);
+    cout << TS->at(heap[x] * 2)+ TS->at(heap[x] * 2 + 1) * tempo  << " ";
+    cout << "(" << TS->at(heap[x] * 2) << " " << TS->at(heap[x] * 2 + 1) << ")" << endl;
+    recursive_print(2 * x + 2, back + 3);
+}
+
+int MaxHeap::size(){
+    return heap.size();
+}
+
+int MaxHeap::sibling(int i){
+    if(i == 0){
+        return -1;
     }
+    if(i % 2 == 0){
+        return i - 1;
+    }
+    return i + 1;
 }
 
 MinHeap::MinHeap() {
@@ -158,12 +181,28 @@ MinHeap::MinHeap(vector<int>* hash_table) {
     min_hash = hash_table;
 }
 
+void MinHeap::troca_interna(int index1, int index2) {
+    cout << "TROCA DO MIN HEAP!!" << endl;
+    for(int i = 0; i < min_hash->size(); i++){
+        cout << "i = " << i << " " << min_hash->at(i) << endl;
+    }
+    cout << "index 1 e 2 = " << index1 << index2 << endl;
+
+    swap(heap[index1], heap[index2]);
+
+    int aux = min_hash->at(index1);
+    min_hash->at(index1) = min_hash->at(index2);
+    min_hash->at(index2) = aux;
+    print_hash();
+}
+
 void MinHeap::insert(int position, int date) {
     Certificate cert;
     cert.position = position;
     cert.expiration_date = date;
     heap.push_back(cert);
     int index = heap.size() - 1;
+    min_hash->push_back(min_hash->size());
 
     while (index > 0) {
         int parent_index = (index - 1) / 2;
@@ -186,19 +225,28 @@ void MinHeap::extractMin() {
     heapify(0);
 }
 
-int MinHeap::getMin() {
+Certificate MinHeap::getMin() {
     if (heap.empty()) {
         throw std::runtime_error("Heap is empty.");
     }
-    return heap[0].position;
+    return heap[0];
 }
 
 bool MinHeap::isEmpty() {
     return heap.empty();
 }
 
-void MinHeap::print() {
-    recursive_print(0, 0);
+void MinHeap::create_certificate(int i){
+    if(this->isEmpty()){
+        this->insert(0, INT_MAX);
+    }
+    else{
+
+    }
+}
+
+void MinHeap::update(int i) {
+
 }
 
 void MinHeap::updateKey(int position, int newExpirationDate) {
@@ -223,6 +271,8 @@ void MinHeap::updateKey(int position, int newExpirationDate) {
         troca_interna(current, (current - 1) / 2);
         current = (current - 1) / 2;
     }
+
+    
 }
 
 void MinHeap::print_cru() {
@@ -251,56 +301,182 @@ void MinHeap::print_max_do_min() {
     maxheap->print();
 }
 
-KinecticHeap::KinecticHeap() {
-    time = 0;
-    maxHeap = MaxHeap(&symbol_table, &max_hash_table, &time);
-    minheap = MinHeap(&min_hash_table);
-    maxHeap.set_min_heap(&minheap);
-    minheap.set_max_heap(&maxHeap);
+void MinHeap::heapify(int index) {
+    int smallest = index;
+    int left_child = 2 * index + 1;
+    int right_child = 2 * index + 2;
+
+    if (left_child < heap.size() && heap[left_child].expiration_date < heap[smallest].expiration_date) {
+        smallest = left_child;
+    }
+
+    if (right_child < heap.size() && heap[right_child].expiration_date < heap[smallest].expiration_date) {
+        smallest = right_child;
+    }
+
+    if (smallest != index) {
+        troca_interna(index, smallest);
+        heapify(smallest);
+    }
 }
 
-void KinecticHeap::Insert_inicial_certificates(int x, int v) {
-    symbol_table.push_back(x);
-    symbol_table.push_back(v);
-    max_hash_table.push_back(max_hash_table.size());
-    maxHeap.add_last_item();
-}
+class KinecticHeap{
+    private:
+    
+    void event(Certificate certificado){
+        int i = certificado.position;
+        int i_sobre_dois = i / 2;
+    
+        if (i < 0 || i_sobre_dois >= maxHeap.size() || i_sobre_dois < 0 || i_sobre_dois >= maxHeap.size()) {
+            throw std::out_of_range("Index out of range.");
+        }
 
-void KinecticHeap::deletar_depois() {
-    minheap.insert(1, 8);
-    minheap.insert(2, 2);
-    minheap.insert(3, 4);
-    minheap.insert(4, 1);
+        int aux = maxHeap.heap[i];
+        maxHeap.heap[i] = maxHeap.heap[i_sobre_dois];
+        maxHeap.heap[i_sobre_dois] = aux;
+    
+        minheap.extractMin();
+        
+        if(i > 1){
+            minheap.update(i_sobre_dois);
+        }
+        minheap.update(i);
+        if(2 * i <= maxHeap.size()){
+            minheap.update(2 * i);
+        }
+        if(2 * i + 1 <= maxHeap.size()){
+            minheap.update(2 * i + 1);
+        }
+        int s = maxHeap.sibling(i);
+        if(s > 0){
+            minheap.update(s);
+        }
+    }
 
-    cout << "MinHeap Min: " << minheap.getMin() << endl;
+    public:
+        vector<int> symbol_table;
+        vector<int> max_hash_table;
+        vector<int> min_hash_table;
+        vector<int> id_hash_table;
+        MaxHeap maxHeap;
+        MinHeap minheap;
+        int time;
 
-    cout << "-----" << endl;
+    KinecticHeap(){
+        time = 0;
+        maxHeap = MaxHeap(&symbol_table, &max_hash_table, &time);
+        minheap = MinHeap(&min_hash_table);
+    }
 
-    minheap.updateKey(4, 12);
+    void insert_inicial_elements(int x, int v){
+        symbol_table.push_back(x);
+        symbol_table.push_back(v);
+        max_hash_table.push_back(max_hash_table.size());
+        maxHeap.add_last_item();
+        maxHeap.print();
+    }
 
-    cout << "MinHeap Updated Min: " << minheap.getMin() << endl;
-}
+    void advance(int t){
+        if(time < t){
+            cout << "O heap cinético não retrocede em tempo!" << endl;
+            return;
+        }
+        while(minheap.getMin().expiration_date <= t){
+            time = minheap.getMin().expiration_date;
+            event(minheap.getMin());
+        }
+        time = t;
+    }
+
+    void change(int id, int v){
+
+    }
+
+    void insert(int id, int xnow, int v){
+        if(time != 0){
+            //fazer tratamento de xnow
+        }
+        else{
+            insert_inicial_elements(xnow, v);
+        }
+    }
+
+    int max(){
+        if(maxHeap.size() == 0){
+            return -1;
+        }
+        return maxHeap.getMax();
+    }
+
+    void delete_max(){
+
+    }
+
+    void delete_id(int id){
+
+    }
+
+    void print(){
+
+    }
+};
+
 
 int main() {
     KinecticHeap Heap_cinetico = KinecticHeap();
     int n, x, v;
     cout << "Escolha o número de elementos a adicionar:";
     cin >> n;
-
-    for (int i = 0; i < n; i++) {
+    
+    for(int i = 0; i < n; i++){
         cin >> x;
         cin >> v;
-        Heap_cinetico.Insert_inicial_certificates(x, v);
+        Heap_cinetico.insert_inicial_elements(x,v);
     }
 
-    for (int i = 0; i < Heap_cinetico.max_hash_table.size(); i++) {
-        cout << "i = " << i << "  =  " << Heap_cinetico.max_hash_table[i] << endl;
+    cout << "Escolha:" << endl;
+    cout << "1 <t>             significa Advance(t)" << endl;
+    cout << "2 <id> <v>        significa Change(id, v)" << endl;
+    cout << "3 <id> <xnow> <v> significa Insert(id, xnow, v)" << endl;
+    cout << "4                 significa Max()" << endl;
+    cout << "5                 significa DeleteMax()" << endl;
+    cout << "6 <id>            significa Delete(id)" << endl;
+    cout << "7                 significa Print()" << endl;
+
+    while(cin >> n){
+        switch (n)
+        {
+        case 1:
+            cin >> x;
+            Heap_cinetico.advance(x);
+            break;
+        case 2:
+            cin >> x;
+            cin >> v;
+            Heap_cinetico.change(x,v);
+            break;
+        case 3:
+            cin >> x;
+            cin >> v;
+            int j;
+            cin >> j;
+            Heap_cinetico.insert(x,v,j);
+            break;
+        case 4:
+            cout << Heap_cinetico.max() << endl;
+            break;
+        case 5:
+            Heap_cinetico.delete_max();
+            break;
+        case 6:
+            cin >> x;
+            Heap_cinetico.delete_id(x);
+        case 7:
+            Heap_cinetico.print();
+        };
     }
-
-    Heap_cinetico.deletar_depois();
-
-    cout << "TESTE TESTE TESTE" << endl;
-    Heap_cinetico.minheap.print_max_do_min();
 
     return 0;
 }
+
+// INT_MIN
--- teste.h
+++ teste.h
@@ -20,16 +20,20 @@ public:
     void print();
     void print_cru();
     void updateKey(int index);
+    void change_key(int id, int new_velocity);
     void add_last_item();
     void set_min_heap(MinHeap* minh);
     void troca_interna(int index1, int index2);
     void recursive_print(int x, int back);
+    int size();
+    int sibling(int i);
+
+    std::vector<int> heap;
 
 private:
     std::vector<int>* TS;
     std::vector<int>* hash;
     int* t;
-    std::vector<int> heap;
     MinHeap* minheap;
     void heapify(int index);  // Adicionei a declaração do método heapify
 };
@@ -38,11 +42,14 @@ class MinHeap {
 public:
     MinHeap();
     MinHeap(std::vector<int>* hash_table);
+    void troca_interna(int index1, int index2);
     void insert(int position, int date);
     void extractMin();
-    int getMin();
+    Certificate getMin();
     bool isEmpty();
     void print();
+    void create_certificate(int i);
+    void update(int i);
     void updateKey(int position, int newExpirationDate);
     void print_cru();
     void print_hash();
@@ -53,10 +60,10 @@ private:
     std::vector<int>* min_hash;
     std::vector<Certificate> heap;
     MaxHeap* maxheap;
-    void heapify(int index);  // Adicionei a declaração do método heapify
+    void heapify(int index);
 };
 
-class KinecticHeap {
+/* class KinecticHeap {
 public:
     KinecticHeap();
     void Insert_inicial_certificates(int x, int v);
@@ -68,6 +75,6 @@ private:
     MinHeap minheap;
     MaxHeap maxHeap;
     int time;
-};
+}; */
 
 int main();
